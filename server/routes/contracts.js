a6f36ad480eb5440082ad9a459ead45fab7f106f
Sergey325 <sabserj2017@gmail.com>
2024-02-17 12:47:44 +0100

    added routes

--- server/routes/contracts.js
+++ server/routes/contracts.js
@@ -1,16 +1,16 @@
-import express from "express";
-import {
-    getAllContacts,
-    addContract,
-} from "../controllers/contracts.js"
-import { verifiedToken } from "../middleware/auth.js";
-
-const router = express.Router()
-
-// READ
-router.get("/", verifiedToken, getAllContacts)
-
-// CREATE
-router.post("/", verifiedToken, addContract)
-
-export default router;
+// import express from "express";
+// import {
+//     getAllContacts,
+//     addContract,
+// } from "../controllers/contracts.js"
+// import { verifiedToken } from "../middleware/auth.js";
+//
+// const router = express.Router()
+//
+// // READ
+// router.get("/", verifiedToken, getAllContacts)
+//
+// // CREATE
+// router.post("/", verifiedToken, addContract)
+//
+// export default router;
